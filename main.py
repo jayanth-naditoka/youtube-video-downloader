8cc9853d5afa013c872e3ad7b02df4a691e823ea
jayanth-naditoka <jayanthnaditoka@gmail.com>
2024-08-30 00:00:42 +0530

    final

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
  
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or specify allowed origins like ["https://example.com"]
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
